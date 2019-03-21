ebaf1a640411ed4ce63d1fd65146e5ff0e2f17cd
Ben Shen <15164268+BenShen98@users.noreply.github.com>
2019-03-21 15:19:00 +0000

    fix ansi

--- test/C_test/func_GE4arg.c
+++ test/C_test/func_GE4arg.c
@@ -1,7 +1,7 @@
 int f6(int a,int b,int c,int d, int e, int f){
-  return f(a,b,c,d,e,f);
+  return ff(a,b,c,d,e,f);
 }
 
-f(int a,int b,int c,int d, int e, int f){
+ff(int a,int b,int c,int d, int e, int f){
   return a+b+c+d+e+f;
 }
